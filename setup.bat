39200440db75f6eb289a9a6868fd747c82e87571
mepro123 <thanaparitwongchanasit@gmail.com>
2026-04-22 15:26:52 +0700

    Update and rename installation.bat to setup.bat

--- setup.bat
+++ setup.bat
@@ -1,16 +1,8 @@
 @echo off
 title Setup Script
 
-echo Requesting admin permissions...
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-echo Run this script as Administrator.
-pause
-exit
-)
-
 echo Creating folder...
-mkdir "C:\Program Files\Games" >nul 2>&1
+mkdir "C:\Games" >nul 2>&1
 
 echo Checking Python...
 python --version >nul 2>&1
